dc8aadc2ce40f0cb06171b3f9f73ae45dfa60e54
davidmijailovic <mijailovicd00@gmail.com>
2023-05-09 21:20:59 +0200

    add appointment button

--- frontend/accomodation-booking/src/app/modules/all-accommodations/all-accommodations.component.ts
+++ frontend/accomodation-booking/src/app/modules/all-accommodations/all-accommodations.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AccommodationService } from 'src/app/services/accommodation/accommodation.service';
 
 @Component({
   selector: 'app-all-accommodations',
   templateUrl: './all-accommodations.component.html',
-  styleUrls: ['./all-accommodations.component.css']
+  styleUrls: ['./all-accommodations.component.css'],
 })
 export class AllAccommodationsComponent implements OnInit {
-  
   public accommodations: any[] = [];
-  
-  constructor(private accommodationService: AccommodationService) {}
+
+  constructor(
+    private accommodationService: AccommodationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.accommodationService.getAccommodations().subscribe((res: any) => {
       this.accommodations = res.accommodations;
-  
     });
   }
 
   changedAutomaticConfirmation(accommodationID: any) {
-    this.accommodationService.changeAutomaticConfirmation(accommodationID).subscribe()
+    this.accommodationService
+      .changeAutomaticConfirmation(accommodationID)
+      .subscribe();
+  }
+
+  addAppointment(accId: string): void {
+    this.router.navigate(['/addAppointment'], {
+      queryParams: {
+        accommodationID: accId,
+      },
+    });
   }
 }
